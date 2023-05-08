eb32fafc29fea6adcc43899c4b184c6798597579
pumchanpom <pum.chanpom@gmail.com>
2023-05-09 03:13:21 +0700

    feat: Add functions to manage homepage

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const restaurantRouter = require("./routes/restaurants");
+const indexRouter = require("./routes");
 const logger = require("./middleware/logger");
 const app = express();
+const path = require("path");
 
 // Template Engine
 const hbs = exphbs.create({ extname: "hbs" });
@@ -10,6 +12,7 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
 // Middleware
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -18,10 +21,7 @@ app.use(logger);
 
 // Routes
 app.use("/apis/restaurants", restaurantRouter);
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/", indexRouter);
 
 app.listen(3000, () => {
   console.log("Listening to Port 3000.");
